e2e6b5c7829c188f6f365e87127b526392f63cc7
Aly M. Ashraf <captainslowg@gmail.com>
2022-10-02 18:29:26 +0200

    iHaveNoClueWhatImDoing1

--- Qc_main.py
+++ Qc_main.py
@@ -1,24 +1,16 @@
 from email.quoprimime import quote
 import typing
-import Qc_data_manip, Qc_excel_manip, Qc_pdf_manip
+import Qc_main_window, Qc_data_manip, Qc_excel_manip, Qc_pdf_manip
 import sys
 from PyQt6 import QtWidgets
 from QuoteCreatorQT.Ui_main_window import Ui_MainWindow
-
-
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    
-    def __init__(self, *args, obj=None, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        self.setupUi(self)
-    #self.vendors=[]
-    
+  
     
     
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
+    window = Qc_main_window.MainWindow()
     window.show()
     app.exec()
 
